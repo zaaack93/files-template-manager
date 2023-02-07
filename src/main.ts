f4d253838fc4bc2c0fa786abe59d94d400372bcd
ZBOUZANGAD <bouzanagdzakaria@gmail.com>
2023-02-07 12:13:53 +0100

    config assets file

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
+import { join } from 'path';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
